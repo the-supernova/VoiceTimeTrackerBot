1798f8f4418b481209244036a8939866f7926456
the-supernova <biswalabhijit04@gmail.com>
2021-10-07 23:03:05 +0530

    Add help commands and minor refactoring

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { prefix, token } = require('./config.json');
+const { MessageEmbed } = require('discord.js');
 const { Client, Intents } = require("discord.js"),
     client = new Client({
         intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] // The GUILD_VOICE_STATES and GUILDS intents are required for discord-voice to function.
@@ -31,8 +32,70 @@ client.on("message", (message) => {
     const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
+    if (command === "help"){
+        const embed = new MessageEmbed()
+            .setTitle("Help")
+            .setDescription("Here is the list of available commands! Use `!v` before them")
+            .setThumbnail("https://i.imgur.com/4KVqtpu.png")
+            .addFields(
+                { name: "create-user", value: "Creates a new user"},
+                { name: "create-config", value: "Creates a new config"},
+                { name: "remove-user", value: "Removes the user from the database and the cache"},
+                { name: "remove-config", value: "Removes the config from the database and the cache"},
+                { name: "edit-user", value: "Edits a user's data\nFor various sub-commands and their arguments, type `edit-user-help`"},
+                { name: "edit-config", value: "Edits a config's data\nFor various sub-commands and their arguments, type `edit-config-help`"},
+                { name: "fetch-id", value: "Get current userId(`fetch-id user <username>`) or guildId(`fetch-id guild`)"},
+                { name: "fetch-user", value: "Get the list of all users(`fetch-user all`), specific to user(`fetch-user user <id>`) or guild(`fetch-user guild <id>`)"},
+                { name: "fetch-config", value: "Get the list of all configs(`fetch-config all`), specific to guild(`fetch-config guild <id>`)"},
+            )
+            .setColor("RANDOM");
+        message.channel.send(embed);
+    }
+    if (command === "edit-user-help"){
+        const embed = new MessageEmbed()
+            .setTitle("Edit User Help")
+            .setDescription("Here is the list of available commands! Use `!v edit-user-help` before them")
+            .setThumbnail("https://i.imgur.com/4KVqtpu.png")
+            .addFields(
+                { name: "xp", value: "Edits a user's xp `Args: Number`"},
+                { name: "level", value: "Edits a user's level `Args: Number`"},
+                { name: "xp-level", value: "Edits a user's xp and level together `Args: Number Number`"},
+                { name: "voice-time", value: "Edits a user's voice time in each channel and total voice time `Args: Number Array`\nSeparate elements with spaces"},
+            )
+            .setColor("RANDOM");
+        message.channel.send(embed);
+    }
+    if (command === "edit-config-help"){
+        const embed = new MessageEmbed()
+            .setTitle("Edit Config Help")
+            .setDescription("Here is the list of available commands! Use `!v edit-config-help` before them")
+            .setThumbnail("https://i.imgur.com/4KVqtpu.png")
+            .addFields(
+                { name: "track-bots", value: "Edits whether bots are able to be tracked `Args: Boolean`"},
+                { name: "track-all-channels", value: "Edits whether to track all of the guild's voice channels `Args: Boolean`"},
+                { name: "exempt-channels", value: "Filters channels not to be tracked `Args: String`"},
+                { name: "channel-ids", value: "The channels to track (if trackAllChannels is true this will be ignored) `Args: Array`\nSeparate elements with spaces"},
+                { name: "exempt-permissions", value: "The user permissions to not track `Args: Array`\nSeparate elements with spaces"},
+                { name: "exempt-members", value: "The user will not be tracked `Args: String`"},
+                { name: "track-mute", value: "Tracks users if they are muted as well `Args: Boolean`"},
+                { name: "track-deaf", value: "Tracks users if they are deafened as well `Args: Boolean`"},
+                { name: "min-user-count-to-participate", value: "The min amount of users to be in a channel to be tracked `Args: Number`"},
+                { name: "max-user-count-to-participate", value: "The max amount of users to be in a channel to be tracked `Args: Number`"},
+                { name: "min-xp-to-participate", value: "The min amount of xp needed to be tracked `Args: Number`"},
+                { name: "max-xp-to-participate", value: "The max amount of xp needed to be tracked `Args: Number`"},
+                { name: "min-level-to-participate", value: "The min level needed to be tracked `Args: Number`"},
+                { name: "max-level-to-participate", value: "The max level needed to be tracked `Args: Number`"},
+                { name: "xp-amount-to-add", value: "The amount of xp to add to the user `Args: Number`"},
+                { name: "voice-time-to-add", value: "The amount of time in ms to add to the user `Args: Number`"},
+                { name: "voice-time-tracking-enabled", value:"Whether the voiceTimeTracking module is enabled `Args: Boolean`"},
+                { name: "leveling-tracking-enabled", value: "Whether the levelingTracking module is enabled `Args: Boolean`"},
+            )
+            .setColor("RANDOM");
+        message.channel.send(embed);
+    }
+
     if (command === "create-user") {
-        message.channel.send("Ok done.");
+        message.channel.send("User added to database..");
         client.voiceManager.createUser(message.author.id, message.guild.id, {
             levelingData: {
                 xp: 0,
@@ -46,7 +109,7 @@ client.on("message", (message) => {
         });
     }
     if (command === "create-config") {
-        message.channel.send("Ok done.");
+        message.channel.send("Config added to database..");
         client.voiceManager.createConfig(message.guild.id, {
             trackBots: false, // If the user is a bot it will not be tracked.
             trackAllChannels: true, // All of the channels in the guild will be tracked.
@@ -69,11 +132,11 @@ client.on("message", (message) => {
         });
     }
     if (command === "remove-user") {
-        message.channel.send("Ok done.");
+        message.channel.send("Removed user from database..");
         client.voiceManager.removeUser(message.author.id, message.guild.id); // Removes the user from the database and the cache.
     }
     if (command === "remove-config") {
-        message.channel.send("Ok done.");
+        message.channel.send("Removed config from database");
         client.voiceManager.removeConfig(message.guild.id); // Removes the config from the database and the cache.
     }
     if (command === "edit-user") {
@@ -85,6 +148,7 @@ client.on("message", (message) => {
                         level: 0
                     }
                 });
+                message.channel.send("User xp updated..");
                 break;
             case "level":
                 client.voiceManager.updateUser(message.author.id, message.guild.id, {
@@ -93,6 +157,7 @@ client.on("message", (message) => {
                         level: parseInt(`${args[1]}`)
                     }
                 });
+                message.channel.send("User level updated..");
                 break;
             case "xp-level":
                 client.voiceManager.updateUser(message.author.id, message.guild.id, {
@@ -101,6 +166,7 @@ client.on("message", (message) => {
                         level: parseInt(`${args[2]}`)
                     }
                 });
+                message.channel.send("User xp and level updated..");
                 break;
             case "voice-time":
                 client.voiceManager.updateUser(message.author.id, message.guild.id, {
@@ -109,6 +175,7 @@ client.on("message", (message) => {
                         total: parseInt(`${args[1]}`)
                     }
                 });
+                message.channel.send("User voice time updated..");
                 break;
             
             default :
@@ -123,101 +190,121 @@ client.on("message", (message) => {
                 client.voiceManager.updateConfig(message.guild.id, {
                     newTrackBots: `${args[1]}` === "true"
                 });
+                message.channel.send("Track bots updated..");
                 break;
             case "track-all-channels":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newTrackAllChannels: `${args[1]}` === "true"
                 });
+                message.channel.send("Track all channels updated..");
                 break;
             case "exempt-channels":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newExemptChannels: (channel) => channel.name === `${args[1]}`
                 });
+                message.channel.send("Exempt channels updated..");
                 break;
             case "channel-ids":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newChannelIDs: `${args.slice(2)}`
                 });
+                message.channel.send("Channel ids updated..");
                 break;
             case "exempt-permissions":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newExemptPermissions: `${args.slice(2)}`
                 });
+                message.channel.send("Exempt permissions updated..");
                 break;
             case "exempt-members":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newExemptMembers: (member) => !member.roles.cache.some((r) => r.name === `${args[1]}`)
                 });
+                message.channel.send("Exempt members updated..");
                 break;
             case "track-mute":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newTrackMute: `${args[1]}` === "true"
                 });
+                message.channel.send("Track mute updated..");
                 break;
             case "track-deaf":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newTrackDeaf: `${args[1]}` === "true"
                 });
+                message.channel.send("Track deaf updated..");
                 break;
             case "min-user-count-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMinUserCountToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Min user count to participate updated..");
                 break;
             case "max-user-count-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMaxUserCountToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Max user count to participate updated..");
                 break;
             case "min-xp-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMinXpToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Min xp to participate updated..");
                 break;
             case "min-level-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMinLevelToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Min level to participate updated..");
                 break;
             case "max-xp-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMaxXpToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Max xp to participate updated..");
                 break;
             case "max-level-to-participate":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newMaxLevelToParticipate: parseInt(`${args[1]}`)
                 });
+                message.channel.send("Max level to participate updated..");
                 break;
             case "xp-amount-to-add":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newXpAmountToAdd: () => parseInt(`${args[1]}`)
                 });
+                message.channel.send("Xp amount to add updated..");
                 break;
             case "voice-time-to-add":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newVoiceTimeToAdd: () => parseInt(`${args[1]}`)
                 });
+                message.channel.send("Voice time to add updated..");
                 break;
             case "voice-time-tracking-enabled":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newVoiceTimeTrackingEnabled: `${args[1]}` === "true"
                 });
+                message.channel.send("Voice time tracking enabled updated..");
                 break;
             case "leveling-tracking-enabled":
                 client.voiceManager.updateConfig(message.guild.id, {
                     newLevelingTrackingEnabled: `${args[1]}` === "true"
                 });
+                message.channel.send("Leveling tracking enabled updated..");
                 break;
             default: 
                 message.channel.send("Invalid argument.");
                 break;
         }
     }
-    if(command === "fetch-id"){
+    if (command === "fetch-id"){
         switch(args[0]){
             case "user":
-                message.channel.send(message.guild.members.find((u) => u.user.username === `${args[1]}`).user.id);
+                message.guild.members.fetch({ query: `${args[1]}`, limit: 1 })
+                .then((u) => { message.channel.send(u.id); })
+                .catch(console.error);
                 break;
             case "guild":
                 message.channel.send(message.guild.id);
@@ -227,7 +314,7 @@ client.on("message", (message) => {
                 break;
         }
     }
-    if(command === "fetch-user"){
+    if (command === "fetch-user"){
         switch(args[0]){
             case "all":
                 message.channel.send(client.voiceManager.users);
@@ -243,12 +330,12 @@ client.on("message", (message) => {
                 break;
         }
     }
-    if(command === "fetch-config"){
+    if (command === "fetch-config"){
         switch(args[0]){
             case "all":
                 message.channel.send(client.voiceManager.configs);
                 break;
-            case "server":
+            case "guild":
                 message.channel.send(client.voiceManager.configs.filter((c) => c.guildId === `${args[1]}`));
                 break;
             default:
